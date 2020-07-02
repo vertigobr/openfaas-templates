d3cd94295a75cd075cbf4002b187fe1746b49132
Felipe Côrtes <contatos.cortes@gmail.com>
2020-07-02 16:24:14 -0300

    Update settings.js

--- template/nodered/function/settings.js
+++ template/nodered/function/settings.js
@@ -62,7 +62,7 @@ module.exports = {
     //debugUseColors: true,
 
     // The file containing the flows. If not set, it defaults to flows_<hostname>.json
-    flowFile: 'flows.json',
+    flowFile: 'flow.json',
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
